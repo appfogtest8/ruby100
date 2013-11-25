344ad07629c838800e8c0b182c2dbcdba3e7ee68
appfogtest8 <appfogtest8@mm.st>
2013-11-24 19:05:36 -0800

    init commit

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+puts "? "
+reverseme = $stdin.gets
+
+x = reverseme.to_i
+
+for i in 1..x
+ if i.remainder(15) == 0
+ 	puts "fizzbuzz"
+ elsif i.remainder(3) == 0
+ 	puts "fizz"
+ elsif i.remainder(5) == 0
+ 	puts "buzz"
+ else
+ 	puts i
+ end
+end
