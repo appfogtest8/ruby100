82a9ee58e95e1ba12787a3dd7b1b8bf8c7c2269c
appfogtest8 <appfogtest8@mm.st>
2014-01-06 00:49:36 -0800

    init commit

---
+++ 146.rb
@@ -0,0 +1,12 @@
+
+get_vals = $stdin.gets
+list_vals = get_vals.split
+
+num_sides = list_vals[0].to_f
+c_radius = list_vals[1].to_f
+
+sidelength = 2 * Math.sin(Math::PI / num_sides) * c_radius
+
+perimeter = sidelength * num_sides
+
+puts "%.3f" % perimeter
